4f44df2834ebac547c687ed6ba1c1e0263559ab0
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-03-15 11:03:36 +0100

    map

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useState } from "react";
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+
+import useStyles from "./styles";
 
 const List = () => {
-    return (
-        <div>
-            <h1>List</h1>
-        </div>
-    );
+  const classes = useStyles();
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
+
+  const places = [
+    { name: "Cool Place" },
+    { name: "Best Beer" },
+    { name: "Best Pizza" },
+  ];
+
+  return (
+    <div className={classes.container}>
+      <Typography variant='h4'>
+        Restaurants, Hôtels & Attractions proches de vous
+      </Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={type} onChange={e => setType(e.target.value)}>
+          <MenuItem value='restaurants'>Restaurants</MenuItem>
+          <MenuItem value='hotels'>Hôtels</MenuItem>
+          <MenuItem value='attractions'>Attractions</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Rating</InputLabel>
+        <Select value={rating} onChange={e => setRating(e.target.value)}>
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={3}>Plus de 3 étoiles</MenuItem>
+          <MenuItem value={4}>Plus de 4 étoiles</MenuItem>
+          <MenuItem value={4.5}>Plus de 4,5 étoiles</MenuItem>
+        </Select>
+      </FormControl>
+      <Grid container spacing={3} className={classes.list}>
+        {/* {place?.map( () => {} )} */}
+      </Grid>
+    </div>
+  );
 };
 
 export default List;
